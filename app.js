bffcbb3c396efc54692f8c4ec4fc000d0db03101
Nelson Segarra <junicus@outlook.com>
2016-03-07 21:10:31 -0400

    basic express application layout, with hello world

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+var routes = require('./routes/index');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+module.exports = app;
